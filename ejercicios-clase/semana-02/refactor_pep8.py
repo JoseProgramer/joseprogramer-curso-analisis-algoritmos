11c258f931f34a699bf6bb227bdad5112cf69885
José Maturana <juanmaturana326671@correo.itm.edu.co>
2026-09-19 18:14:51 -0500

    Refactoriza script de promedio aplicando PEP 8

--- ejercicios-clase/semana-02/refactor_pep8.py
+++ ejercicios-clase/semana-02/refactor_pep8.py
@@ -1,8 +1,26 @@
-def CalcularPromedio(Lista):
-    s=0
-    for x in Lista:
-     s=s+x
-    return s/len(Lista)
- 
-l=[1,2,3,4,5]
-print(CalcularPromedio(l))
+def calcular_promedio(lista_numeros: list[float]) -> float:
+    """
+    Calcula el promedio de una lista de números.
+
+    Args:
+        lista_numeros: Lista de valores numéricos.
+
+    Returns:
+        Promedio de los valores recibidos.
+    """
+
+    suma = 0
+
+    for numero in lista_numeros:
+        suma = suma + numero
+
+    return suma / len(lista_numeros)
+
+
+def main() -> None:
+    numeros = [1, 2, 3, 4, 5]
+    print(calcular_promedio(numeros))
+
+
+if __name__ == "__main__":
+    main()
